e311f8aed6fe48f4c19be255ba7771d2200fa154
AnadelaMata28 <ana.delamatadelai@gmail.com>
2024-04-16 10:32:41 +0200

    productos

---
+++ Solucion/Solucion/MaterialesPreciosos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion
+{
+    internal class MaterialesPreciosos : Producto
+    {
+        public string TipoMaterial {  get; set; }
+        public double Peso {  get; set; }
+        public MaterialesPreciosos(string tipoMaterial, double peso) : base
+    }
+}
---
+++ Solucion/Solucion/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion
+{
+    internal class Producto
+    {
+        public string Nombre {  get; set; }
+        public int Unidades { get; set; }
+        public double PrecioUnitario { get; set; }
+        public string Descripcion { get; set; }
+
+        public Producto() { }
+        public Producto(string nombre, int unidades, double precioUnitario, string descripcion)
+        {
+            Nombre = nombre;
+            Unidades = unidades;
+            PrecioUnitario = precioUnitario;
+            Descripcion = descripcion;
+        }
+    }
+}
