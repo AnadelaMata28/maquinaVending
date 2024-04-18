ccb1731d7b8699b5fca2afe723f49a62b618b697
Mariaafazio <mariafazioa52@gmail.com>
2024-04-18 13:00:05 +0200

    program

--- Solucion/Solucion/Program.cs
+++ Solucion/Solucion/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +11,69 @@ namespace Solucion
     {
         static void Main(string[] args)
         {
+            string contraseña = 'admin'; 
+
             int opcion = 0;
-            Console.WriteLine("Introduzca la opción que desee: ");
-            Console.WriteLine("1. Comprar productos");
-            Console.WriteLine("2. Mostrar la información del producto");
-            Console.WriteLine("3. Carga individual del producto");
-            Console.WriteLine("4. Carga completa de productos");
-            Console.WriteLine("5. Salir");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("|           Máquina Vending UFV           |");
+            Console.WriteLine("|-----------------------------------------|"); 
+            Console.WriteLine("|  Opciones:                              |");
+            Console.WriteLine("|-----------------------------------------|");
+            Console.WriteLine("| 1. Comprar productos                    |");
+            Console.WriteLine("| 2. Mostrar la información del producto  |");
+            Console.WriteLine("| 3. Carga individual del producto        |");
+            Console.WriteLine("| 4. Carga completa de productos          |");
+            Console.WriteLine("| 5. Salir                                |");
+            Console.WriteLine("|-----------------------------------------|");
+            Console.WriteLine("| Introduzca la opción que desee realizar |")
+            Console.WriteLine("------------------------------------------");
             opcion = int.Parse(Console.ReadLine());
 
             switch(opcion) {
-                case 1: // Comprar
+                case 1:
+                    BuyProducts(); // Comprar
                     break;
-                case 2: // Información
+                case 2:
+                    ProductInfo(int Id); // Información
                     break; 
-                case 3: // Carga individual
+                case 3:
+                    if (ContrasenaValida())
+                    {
+                        IndividualProductLoading();// Carga individua
+                    }
+                    else
+                    {
+                        Console.WriteLine("La contraseña es incorrecta"); 
+                    }
                     break;
-                case 4: // Carga completa
+                case 4:
+                    if (ContrasenaValida())
+                    {
+                        FullProductLoading();// Carga completa
+                    }
+                    else
+                    {
+                        Console.WriteLine("La contraseña es incorrecta");
+                    }
                     break;
-                case 5: // Exit
+                case 5:
+                    Exit(); // Exit
                     break;
                 default:
                     break;
             }
+
         }
-    }
+
+        public bool ContrasenaValida()
+        {
+            bool valido = false; 
+            Console.WriteLine("Introduzca la contraseña: ");
+            string contraseñaIntroducida = Console.ReadLine();
+            if (contraseñaIntroducida == contraseña)
+            {
+                valido = true; 
+            }
+            return valido; 
+        }
 }
