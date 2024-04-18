ec9924fba3eff451c220bb6aadae0c0646563a75
Natalia Berbel Ochoa <nberoc@gmail.com>
2024-04-18 14:17:33 +0200

    actualizacion

--- Solucion/Solucion/MaquinaVending.cs
+++ Solucion/Solucion/MaquinaVending.cs
@@ -11,6 +11,7 @@ namespace Solucion {
         public string Nombre { get; set; }
         public int UnidadesDisponibles { get; set; }
         public int Precio { get; set; }
+        List <Producto> Productos { get; set; }
 
         public MaquinaVending() { }
 
@@ -28,23 +29,23 @@ namespace Solucion {
 
             int opcion = 0;
             Console.WriteLine("Desea comprar algún otro producto?");
-            Console.Write("En caso de querer, pulse 1. En caso contrario, pulse 0 ")
+            Console.Write("En caso de no querer, pulse 0. Si desea compras más, pulse 1: ");
             opcion = int.Parse(Console.ReadLine());
             do {
                 switch (opcion) {
-                    case 1:
+                    case 0:
                         Console.WriteLine(ProductInfo(Id));
                         Exit();
                         break;
-                    case 2:
+                    case 1:
                         Console.WriteLine("Introduzca el ID del producto que desee comprar: ");
                         Id = int.Parse(Console.ReadLine());
                         Console.WriteLine(ProductInfo(Id));
                         break;
                     default:
                         break;
-                } while (opcion != 3) ;
-            } while (opcion == 1);
+                }
+            } while (opcion != 1);
         }
 
         public void ProductInfo(int Id) {
@@ -65,5 +66,3 @@ namespace Solucion {
     }
 }
      
-
-    
