b744e8ebd092228b8afc02dd56437f2e9ce79349
Natalia Berbel Ochoa <nberoc@gmail.com>
2024-04-18 11:40:32 +0200

    maquina

--- Solucion/Solucion/MaquinaVending.cs
+++ Solucion/Solucion/MaquinaVending.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Solucion
-{
-    internal class MaquinaVending
-    {
+namespace Solucion {
+    internal class MaquinaVending {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int UnidadesDisponibles { get; set; }
+        public int Precio { get; set; }
+
+        public MaquinaVending() { }
+
+        public MaquinaVending(int id, string nombre, int unidadesDisponibles, int precio) {
+            Id = id;
+            Nombre = nombre;
+            UnidadesDisponibles = unidadesDisponibles;
+            Precio = precio;
+        }
+
+        public void BuyProducts() {
+            Console.WriteLine("Introduzca el ID del producto que desee comprar: ");
+            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine(ProductInfo(Id));
+
+            int opcion = 0;
+            Console.WriteLine("Desea comprar algún otro producto?");
+            Console.Write("En caso de querer, pulse 1. En caso contrario, pulse 0 ")
+            opcion = int.Parse(Console.ReadLine());
+            do {
+                switch (opcion) {
+                    case 1:
+                        Console.WriteLine(ProductInfo(Id));
+                        Exit();
+                        break;
+                    case 2:
+                        Console.WriteLine("Introduzca el ID del producto que desee comprar: ");
+                        Id = int.Parse(Console.ReadLine());
+                        Console.WriteLine(ProductInfo(Id));
+                        break;
+                    default:
+                        break;
+                } while (opcion != 3) ;
+            } while (opcion == 1);
+        }
+
+        public void ProductInfo(int Id) {
+
+        }
+
+        public void IndividualProductLoading() {
+
+        }
+
+        public void FullProductLoading() {
+
+        }
+
+        public void Exit() {
+            Console.WriteLine("Muchas gracias por su compra!");
+        }
     }
 }
+     
+
+    
--- Solucion/Solucion/Program.cs
+++ Solucion/Solucion/Program.cs
@@ -10,6 +10,29 @@ namespace Solucion
     {
         static void Main(string[] args)
         {
+            int opcion = 0;
+            Console.WriteLine("Introduzca la opción que desee: ");
+            Console.WriteLine("1. Comprar productos");
+            Console.WriteLine("2. Mostrar la información del producto");
+            Console.WriteLine("3. Carga individual del producto");
+            Console.WriteLine("4. Carga completa de productos");
+            Console.WriteLine("5. Salir");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch(opcion) {
+                case 1: // Comprar
+                    break;
+                case 2: // Información
+                    break; 
+                case 3: // Carga individual
+                    break;
+                case 4: // Carga completa
+                    break;
+                case 5: // Exit
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
