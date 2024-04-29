c5ce2b04c2efd0571213c1dfb82715560af0d504
Natalia Berbel Ochoa <nberoc@gmail.com>
2024-04-29 20:01:18 +0200

    actualizar

--- Solucion/Solucion/Pagar.cs
+++ Solucion/Solucion/Pagar.cs
@@ -8,31 +8,51 @@ namespace Solucion {
     internal class Pagar {
         public Pagar() { }
 
-        public string PagarEfectivo(double precioTotal) 
+        public string PagarEfectivo(double precioTotal)
         {
-            double dinero = 0;
+            try
+            {
+                double dinero = 0;
+                Console.WriteLine();
+                Console.WriteLine($"Cantidad a pagar {precioTotal}");
+                do
+                {
+                    Console.WriteLine("Introduzca la cantidad requerida en euros por favor: ");
+                    dinero = double.Parse(Console.ReadLine());
+                } while (dinero < precioTotal);
 
-            Console.WriteLine($"Cantidad a pagar: {precioTotal}");
-            do { 
-                Console.WriteLine("Introduzca la cantidad requerida en euros por favor: ");
-                dinero = double.Parse(Console.ReadLine());
-            } while (dinero < precioTotal);
-
-            DevolverDinero (precioTotal, dinero);
-            return $"Pagado con éxito";
-
+                DevolverDinero(precioTotal, dinero);
+                return $"Pagado con éxito";
+            }
+            catch(FormatException) {
+                return "Error: Opción inválida. Por favor, ingrese un número válido.";
+            }
         }
 
         public void PagarTarjeta(double precioTotal) {
-            Console.WriteLine($"Cantidad a pagar: {precioTotal}"); 
-            Console.WriteLine("Número de tarjeta: ");
-            int numeroTarjeta = int.Parse(Console.ReadLine());
-            Console.WriteLine("Fecha de caducidad: ");
-            string fechaCaducidad = Console.ReadLine();
-            Console.WriteLine("Código de seguridad: ");
-            int codigoSeguridad = int.Parse(Console.ReadLine());
-            Console.ReadKey();
-            Console.WriteLine("Pagando....");
+            try
+            {
+                Console.WriteLine(); 
+                Console.WriteLine($"Cantidad a pagar {precioTotal}");
+                Console.WriteLine("Número de tarjeta: ");
+                int numeroTarjeta = int.Parse(Console.ReadLine());
+                Console.WriteLine("Fecha de caducidad: ");
+                string fechaCaducidad = Console.ReadLine();
+                Console.WriteLine("Código de seguridad: ");
+                int codigoSeguridad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Pagando....");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Opción inválida. Por favor, ingrese un número válido.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(); 
+            Console.WriteLine("Transacción realizada con éxito. Gracias por su compra!");
+
         }
 
         public void DevolverDinero(double precioTotal, double dinero)
@@ -43,11 +63,18 @@ namespace Solucion {
             {
                 cambio = precioTotal - dinero;
 
-                double centimos = (cambio*100)% 100;
-                double euros = cambio - (centimos/100); 
-                 Console.WriteLine($"Su vuelta son {euros} euros y {centimos} céntimos.");
-
-                Console.WriteLine($"Su vuelta son {cambio} euros.");
+                double centimos = -1*(cambio*100)% 100;
+                double euros = -1*(cambio - (centimos/100)); 
+                if (centimos == 0)
+                {
+                    Console.WriteLine($"Su vuelta: {euros} euros.");
+                }
+                else 
+                {
+                    Console.WriteLine($"Su vuelta son {euros} euros y {centimos} centimos.");
+                }
+                Console.WriteLine(); 
+                Console.WriteLine("Gracias por su compra!"); 
             }
         }
     }
